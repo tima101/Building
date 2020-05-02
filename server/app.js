0830b0b6c2036cf62554adab4e4504ebf1ed300e
mrhanson3 <cahanson12@gmail.com>
2020-05-01 20:33:27 -0400

    Added Express server

--- server/app.js
+++ server/app.js
@@ -7,5 +7,5 @@ server.get("/", (req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log("Ready on port 3000!"); //eslint-disable-line no-console
+  console.log("Ready on port 3000!"); // eslint-disable-line no-console
 });
